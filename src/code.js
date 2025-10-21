c5bb177a4319cab3d191659906ab444eb5173dac
Puneeth Chaganti <punchagan@muse-amuse.in>
2025-10-21 11:19:19 +0530

    Remove GAS code to update file IDs

--- src/code.js
+++ src/code.js
@@ -92,52 +92,5 @@ const getColumnNumbers = (sheet) => {
 
 const onOpen = () => {
   const ui = SpreadsheetApp.getUi();
-  ui.createMenu("Website")
-    .addItem("Update File IDs", "update")
-    .addSeparator()
-    .addItem("Help", "showInstructions")
-    .addToUi();
-};
-
-const update = () => {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  const data = sheet.getDataRange().getValues();
-
-  const props = PropertiesService.getScriptProperties();
-  const folderId = props.getProperty("FOLDER_ID");
-  if (!folderId) {
-    SpreadsheetApp.getUi().alert("Folder ID not set in script properties.");
-    return;
-  }
-  const folder = DriveApp.getFolderById(folderId);
-  const files = folder.getFiles();
-  const fileMap = {};
-
-  const { fileNameColumn, fileIdColumn } = getColumnNumbers(sheet);
-
-  // Map file name to file object (not just ID, we need to update permissions)
-  while (files.hasNext()) {
-    const file = files.next();
-    fileMap[file.getName()] = file;
-  }
-
-  for (let i = 1; i < data.length; i++) {
-    const filename = data[i][fileNameColumn - 1];
-    const range = sheet.getRange(i + 1, fileIdColumn);
-
-    if (filename && fileMap[filename]) {
-      const file = fileMap[filename];
-
-      // Set sharing permission
-      file.setSharing(
-        DriveApp.Access.ANYONE_WITH_LINK,
-        DriveApp.Permission.VIEW,
-      );
-
-      // Insert file ID
-      range.setValue(file.getId());
-    } else if (filename) {
-      range.setValue("❌ Not Found");
-    }
-  }
+  ui.createMenu("Website").addItem("Help", "showInstructions").addToUi();
 };
